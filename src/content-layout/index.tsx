ab49bb38a7b6017482eff4f5378c8b2000eef4d3
Scott O'Brien <scottob@amazon.com>
2022-07-28 14:34:11 -0400

    Incorporated PR feedback.

--- src/content-layout/index.tsx
+++ src/content-layout/index.tsx
@@ -1,18 +1,26 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import React, { useContext, useLayoutEffect } from 'react';
+import React, { useContext, useLayoutEffect, useRef } from 'react';
 import clsx from 'clsx';
 import { AppLayoutContext } from '../app-layout/visual-refresh/context';
 import { ContentLayoutProps } from './interfaces';
+import { getBaseProps } from '../internal/base-component';
+import useBaseComponent from '../internal/hooks/use-base-component';
 import { useContainerQuery } from '../internal/hooks/container-queries';
+import { useMergeRefs } from '../internal/hooks/use-merge-refs';
 import { useVisualRefresh } from '../internal/hooks/use-visual-mode';
 import styles from './styles.css.js';
 
 export { ContentLayoutProps };
 
-export default function ContentLayout({ children, disableOverlap, header }: ContentLayoutProps) {
+export default function ContentLayout({ children, disableOverlap, header, ...rest }: ContentLayoutProps) {
+  const baseProps = getBaseProps(rest);
   const { breadcrumbs, setDynamicOverlapHeight } = useContext(AppLayoutContext);
-  const isVisualRefresh = useVisualRefresh();
+  const rootElement = useRef<HTMLDivElement>(null);
+  const { __internalRootRef } = useBaseComponent('ContentLayout');
+  const mergedRef = useMergeRefs(rootElement, __internalRootRef);
+  const isVisualRefresh = useVisualRefresh(rootElement);
+
   const isOverlapDisabled = !children || !header || disableOverlap;
 
   // Documentation to be added.
@@ -29,10 +37,12 @@ export default function ContentLayout({ children, disableOverlap, header }: Cont
 
   return (
     <div
-      className={clsx(styles.layout, {
+      {...baseProps}
+      className={clsx(baseProps.className, styles.layout, {
         [styles['is-overlap-disabled']]: isOverlapDisabled,
         [styles['is-visual-refresh']]: isVisualRefresh,
       })}
+      ref={mergedRef}
     >
       <div
         className={clsx(
